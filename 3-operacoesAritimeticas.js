594f715192fb0007209874a74655a8aded7347cd
Marcos crispim <99744094+MarcosCrispim@users.noreply.github.com>
2022-02-21 15:52:57 -0300

    Minha evolução

---
+++ 3-operacoesAritimeticas.js
@@ -0,0 +1,4 @@
+console.log("operações aritimeticas");
+
+console.log(2+2);
+console.log((10+8)*2)
---
+++ 4-conversao.js
@@ -0,0 +1,10 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2022);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("9" / "2");// podemos fazer operações com ponto flutuante, ou seja quando queremos usar a virgula usamos o ponto no seu lugar 
+console.log("ricardo" / "2"); //Not a Number NaN
+// na divisão vai dar certo pois no js ele so consegue dividir numeros não palavras , ja na adiçãodapra fazer uma concadenação
+
